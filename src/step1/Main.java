cdb25d7ef162b21276d0926f7482cef70c1f088e
lecture <Robbie>
2023-04-16 21:06:03 +0900

    Revert "1 step 완료"
    This reverts commit e9a975a8717f8858d4e2228648e163a0400f11a2.

--- src/step1/Main.java
+++ src/step1/Main.java
@@ -1,34 +1,8 @@
 package step1;
 
 
-import step1.app.Consumer;
-import step1.app.OrderApplication;
-import step1.enums.Menu;
-import step1.order.Order;
-import step1.order.OrderList;
-
 public class Main {
     public static void main(String[] args) {
-        //치킨 주문
-        Consumer consumer = new OrderApplication();
-        consumer.chickenOrder(Menu.Chicken,20);
 
-        //주문 확인
-        OrderList orderList = new OrderList();
-        for (Order order : orderList.getOrderList()) {
-            System.out.println(order.getFoodName());
-            System.out.println(order.getPrice());
-            System.out.println(order.getAmount());
-            System.out.println();
-        }
-        consumer.pizzaOrder(Menu.Pizza, 2);
-
-        // 주문 내역 확인
-        orderList = new OrderList();
-        for (Order order : orderList.getOrderList()) {
-            System.out.println(order.getFoodName());
-            System.out.println(order.getPrice());
-            System.out.println(order.getAmount());
-        }
     }
 }
--- src/step1/order/OrderList.java
+++ src/step1/order/OrderList.java
@@ -1,16 +1,5 @@
 package step1.order;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class OrderList {
-    private static List<Order> orderList = new ArrayList<>();
 
-    public void setOrderList(Order order) {
-        orderList.add(order);
-    }
-
-    public static List<Order> getOrderList() {
-        return orderList;
-    }
 }
