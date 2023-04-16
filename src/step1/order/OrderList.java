8d60c3320e26ddbec2e7d5a9409290fcab31422b
lecture <Robbie>
2023-04-16 21:03:46 +0900

    1 step 완료 - 주문 데이터 받기 - 주문 데이터 List에 저장 - 주문 별 메뉴 이름, 가격, 갯수 출력

--- src/step1/order/OrderList.java
+++ src/step1/order/OrderList.java
@@ -1,5 +1,16 @@
 package step1.order;
 
-public class OrderList {
+import java.util.ArrayList;
+import java.util.List;
 
+public class OrderList {
+    private static List<Order> orderList = new ArrayList<>();
+
+    public void setOrderList(Order order) {
+        orderList.add(order);
+    }
+
+    public static List<Order> getOrderList() {
+        return orderList;
+    }
 }
